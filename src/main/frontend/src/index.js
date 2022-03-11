718ead8a9a9a8d708213d49121ee2b75b545d47d
jacobvetere <jvetere1999@gmail.com>
2022-03-11 16:15:20 -0500

    Testing Added operation code Debugging JS Debugging maven Refactoring
    Signed-off-by: jacobvetere <jvetere1999@gmail.com>

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import reportWebVitals from './tests/reportWebVitals';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
