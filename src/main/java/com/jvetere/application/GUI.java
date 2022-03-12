3dd1ea8877859d004290000c7d9db62124c134d8
jacob <jvetere1999@gmail.coms>
2022-03-12 10:47:07 -0500

    rga

--- src/main/java/com/jvetere/application/GUI.java
+++ src/main/java/com/jvetere/application/GUI.java
@@ -1,26 +1,26 @@
-package com.jvetere.application;
-
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class GUI  {
-//    @Override
-//    public void start(Stage stage) {
-//        DropBox drop = new DropBox("Drop File Here");
+//package com.jvetere.application;
 //
 //
-//        StackPane root = new StackPane();
-//        root.getChildren().add(drop.me);
-//        Scene scene = new Scene(root, 500, 250);
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.scene.layout.StackPane;
+//import javafx.stage.Stage;
 //
-//        stage.setTitle("Drag image");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//    public static void main (String[] args) {
-//        launch(args);
-//    }
-}
+//public class GUI  {
+////    @Override
+////    public void start(Stage stage) {
+////        DropBox drop = new DropBox("Drop File Here");
+////
+////
+////        StackPane root = new StackPane();
+////        root.getChildren().add(drop.me);
+////        Scene scene = new Scene(root, 500, 250);
+////
+////        stage.setTitle("Drag image");
+////        stage.setScene(scene);
+////        stage.show();
+////    }
+////    public static void main (String[] args) {
+////        launch(args);
+////    }
+//}
