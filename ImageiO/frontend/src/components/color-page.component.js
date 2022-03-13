02017da83c77200996315546df39ff9b088d976c
jacobvetere <jvetere1999@gmail.com>
2022-03-13 19:30:09 -0400

    refactor

---
+++ ImageiO/frontend/src/components/color-page.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import colorPageService from "../service/color-page.service";
+
+export default class ColorPage extends Component {
+    render() {
+       return(
+       <div>
+            
+       </div>
+       );
+    }
+}
---
+++ ImageiO/frontend/src/service/color-page.service.js
@@ -0,0 +1,8 @@
+import http from "../http-common";
+
+class ColorJsonService {
+    getJson() {
+       
+    }
+}
+export default new ColorJsonService();
