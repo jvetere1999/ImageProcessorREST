48e047650a22fb46987ad37efe49f55dded5d84a
jacobvetere <jvetere1999@gmail.com>
2022-03-13 22:15:26 -0400

    refactor

--- ImageiO/frontend/src/App.js
+++ ImageiO/frontend/src/App.js
@@ -1,4 +1,4 @@
-waimport React, { Component } from 'react';
+import React, { Component } from 'react';
 import "./style/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UploadImages from './components/image-upload.component';
