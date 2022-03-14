e640f6a332d481d7a44394088fdc85239fa6ada8
jacobvetere <jvetere1999@gmail.com>
2022-03-14 19:14:13 -0400

    refactor

---
+++ ImageiO/frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./style/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UploadFiles from './components/upload-files.component';
+import UploadImages from './components/image-upload.component';
+
+
+function App()  {
+  return (
+    <div className='container' style={{ width: "600px" }}>
+      <div style={{ margin: "20px" }}>
+        <h3>ImageProcessor</h3>
+        <h4>Upload Files</h4>
+      </div>
+      <PixelGrid
+                data={Array(64).fill(0).map(Math.random)}
+                options={{
+                size: 10,
+                padding: 2,
+                background: [0, 0.5, 1],
+                }}
+            />
+    </div>
+    );
+  
+}
+
+
+export default App;
