6151b98b8c40dbffaccec2ca83dada46dd8d1144
jacobvetere <jvetere1999@gmail.com>
2022-03-14 19:33:07 -0400

    refactor

--- ImageiO/frontend/src/App.js
+++ ImageiO/frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./style/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import UploadImages from './components/image-upload.component';
-import ColorPage from './components/color-page.component';
+import UploadImages from './components/image-upload.component';;
+import PixelGrid from "react-pixel-grid";
 
 
 function App() {
@@ -13,6 +13,14 @@ function App() {
           <h4>Upload Files</h4>
         </div>
         <UploadImages/>
+          <PixelGrid
+              data={Array(64).fill(0).map(Math.random)}
+              options={{
+                  size: 10,
+                  padding: 2,
+                  background: [0, 0.5, 1],
+              }}
+          />
       </div>
       );
 }
