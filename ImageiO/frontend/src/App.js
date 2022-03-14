4c4bd4a47f4b6e1b330010c2fc5a74157dac719d
jacobvetere <jvetere1999@gmail.com>
2022-03-14 19:27:57 -0400

    refactor

--- ImageiO/frontend/src/App.js
+++ ImageiO/frontend/src/App.js
@@ -6,38 +6,14 @@ import ColorPage from './components/color-page.component';
 
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      curremtComponent: 'upload',
-    };
-  }
-  switchComponent(comp) {
-    this.setState({
-      curremtComponent: comp,
-    })
-  }
-  getComponent(){
-    let component;
-    switch(this.state.curremtComponent){
-      case 'upload':
-        component = <UploadImages/>
-        break;
-      case 'grid':
-        component = <ColorPage/>
-        break;
-    }
-    return component;
-  }
   render(){
     return (
       <div className='container' style={{ width: "600px" }}>
         <div style={{ margin: "20px" }}>
-            <ColorPage />
           <h3>ImageProcessor</h3>
           <h4>Upload Files</h4>
         </div>
+        <ColorPage/>
         <UploadImages/>
       </div>
       );
