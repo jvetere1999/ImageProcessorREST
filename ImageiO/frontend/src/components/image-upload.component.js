8ab4a0e96c06db2ab158f30dce102cc74fd3a049
jacobvetere <jvetere1999@gmail.com>
2022-03-14 22:21:16 -0400

    refactor

--- ImageiO/frontend/src/components/image-upload.component.js
+++ ImageiO/frontend/src/components/image-upload.component.js
@@ -119,8 +119,7 @@ export default class UploadImages extends Component {
                         {imageInfos &&
                             imageInfos.map((img, index) => (
                                 <li className="list-group-item" key={index}>
-                                    <Link to={`/files/${img.url}`} 
-                                    onClick={(e) => this.imageGet(e, img.url)}>{img.name}</Link> 
+                                    <Link to={`/files/${img.name}`} >{img.name}</Link> 
                                 </li>
                             ))}
                     </ul>
